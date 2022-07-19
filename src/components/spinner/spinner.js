b87e1380a6244540e59d3986b62c0de7e04b95e3
Boris <boris_galunga@mail.ru>
2022-07-19 20:05:26 +0300

    create react.fragment + transfrom data

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,14 @@
+import react from "react";
+import "./spinner.css";
+const Spinner = () => {
+	return (
+		<div class="loadingio-spinner-radio-akknwr4hnz">
+			<div class="ldio-x5osy0kb6wp">
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+	);
+};
+export default Spinner;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,5 @@
-class SwapiService {
-	_apiBase = "https://swapi.dev/api";
-	async getResource(url) {
-		const res = await fetch(`${this._apiBase}${url}`);
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app";
 
-		if (!res.ok) {
-			throw new Error(`Could not fetch ${url}` + `recieved ${res.status}`);
-		}
-		return await res.json();
-	}
-	async getAllPeople() {
-		const res = await this.getResource("/people/");
-		return res.results;
-	}
-	getAllPerson(id) {
-		return this.getResource(`/people/${id}`);
-	}
-}
-
-const swapi = new SwapiService();
-
-swapi.getAllPeople().then((res) => {
-	res.forEach((element) => {
-		console.log(element.name);
-	});
-});
+ReactDOM.render(<App />, document.getElementById("root"));
