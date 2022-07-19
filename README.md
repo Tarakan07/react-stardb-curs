720c20bb08b44892876baddf4ff742b504636385
Boris <boris_galunga@mail.ru>
2022-07-19 13:27:32 +0300

    fetch api, examples

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+-Axios
+-Superagent
+-Got
+-Request
+-Reqwest
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+class SwapiService {
+	_apiBase = "https://swapi.dev/api";
+	async getResource(url) {
+		const res = await fetch(`${this._apiBase}${url}`);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+		if (!res.ok) {
+			throw new Error(`Could not fetch ${url}` + `recieved ${res.status}`);
+		}
+		return await res.json();
+	}
+	async getAllPeople() {
+		const res = await this.getResource("/people/");
+		return res.results;
+	}
+	getAllPerson(id) {
+		return this.getResource(`/people/${id}`);
+	}
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const swapi = new SwapiService();
+
+swapi.getAllPeople().then((res) => {
+	res.forEach((element) => {
+		console.log(element.name);
+	});
+});
