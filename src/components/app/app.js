db4b14d5368f8a7161b89e643356012e905c8838
Boris <boris_galunga@mail.ru>
2022-07-21 00:41:11 +0300

    использовали паттерн-функцию: передаеми в item-list функцию, и теперь item-list универсальный

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,9 +2,10 @@ import React, { Component } from "react";
 
 import Header from "../header";
 import RandomPlanet from "../random-planet";
-
+import ItemList from "../item-list";
+import PersonDetails from "../person-details";
 import PeoplePage from "../people-page";
-
+import SwapiService from "../../services/swapi-service";
 import "./app.css";
 import ErrorIndicator from "../error-indicator";
 
@@ -13,7 +14,7 @@ export default class App extends Component {
 		selectedPerson: 5,
 		hasError: false,
 	};
-
+	swapiService = new SwapiService();
 	componentDidCatch() {
 		this.setState({ hasError: true });
 	}
@@ -28,8 +29,30 @@ export default class App extends Component {
 				<RandomPlanet />
 
 				<PeoplePage />
-				<PeoplePage />
-				<PeoplePage />
+
+				<div className="row mb2 row-data">
+					<div className="col-md-6">
+						<ItemList
+							onItemSelected={this.onPersonSelected}
+							getData={this.swapiService.getAllPlanets}
+						/>
+					</div>
+					<div className="col-md-6">
+						<PersonDetails personId={this.state.selectedPerson} />
+					</div>
+				</div>
+
+				<div className="row mb2 row-data">
+					<div className="col-md-6">
+						<ItemList
+							onItemSelected={this.onPersonSelected}
+							getData={this.swapiService.getAllStarships}
+						/>
+					</div>
+					<div className="col-md-6">
+						<PersonDetails personId={this.state.selectedPerson} />
+					</div>
+				</div>
 			</div>
 		);
 	}
--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import ItemList from "../item-list";
 import PersonDetails from "../person-details";
 import ErrorIndicator from "../error-indicator";
+import SwapiService from "../../services/swapi-service";
 import "./people-page.css";
 export default class PeoplePage extends Component {
 	state = {
 		selectedPerson: 3,
 		hasError: false,
 	};
+	swapiService = new SwapiService();
 	onPersonSelected = (id) => {
 		this.setState({ selectedPerson: id });
 	};
@@ -19,9 +21,12 @@ export default class PeoplePage extends Component {
 			return <ErrorIndicator />;
 		}
 		return (
-			<div className="row mb2">
+			<div className="row mb2 row-data">
 				<div className="col-md-6">
-					<ItemList onItemSelected={this.onPersonSelected} />
+					<ItemList
+						onItemSelected={this.onPersonSelected}
+						getData={this.swapiService.getAllPeople}
+					/>
 				</div>
 				<div className="col-md-6">
 					<PersonDetails personId={this.state.selectedPerson} />
