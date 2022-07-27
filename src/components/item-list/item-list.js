a696b007c7a5c5037c685e76b8dce5093d67ccf2
Boris <boris_galunga@mail.ru>
2022-07-27 22:00:59 +0300

    again

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,31 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import { withData } from '../hoc-helpers';
+import { withData } from "../hoc-helpers";
 import SwapiService from "../../services/swapi-service";
-import './item-list.css';
+import "./item-list.css";
 
 const ItemList = (props) => {
+	const { data, onItemSelected, children: renderLabel } = props;
 
-  const { data, onItemSelected, children: renderLabel } = props;
+	const items = data.map((item) => {
+		const { id } = item;
+		const label = renderLabel(item);
 
-  const items = data.map((item) => {
-    const { id } = item;
-    const label = renderLabel(item);
+		return (
+			<li
+				className="list-group-item"
+				key={id}
+				onClick={() => onItemSelected(id)}
+			>
+				{label}
+			</li>
+		);
+	});
 
-    return (
-      <li className="list-group-item"
-          key={id}
-          onClick={() => onItemSelected(id)}>
-        {label}
-      </li>
-    );
-  });
-
-  return (
-    <ul className="item-list list-group">
-      {items}
-    </ul>
-  );
+	return <ul className="item-list list-group">{items}</ul>;
 };
 
 const { getAllPeople } = new SwapiService();
