22da513ca9a12590b0636204418d6fd9ec5d85eb
Boris <@server.lv>
2022-11-14 10:54:24 +0300

    withRouter()-компонент высшего порядка, оборавичвает компонент и передает ему параметры route. С помощью history(объект) пушим url подстраницы и уже в app в match можно вынимать продолжение ссылки (id)

--- src/components/pages/starship-page.js
+++ src/components/pages/starship-page.js
@@ -1,23 +1,13 @@
-import { render } from "@testing-library/react";
-import React, { Component } from "react";
-import Row from "../row/row";
-import { StarshipDetails, StarshipList } from "../sw-components";
-export default class StarshipPage extends Component {
-	state = {
-		selectedItem: null,
-	};
-	onItemSelected = (selectedItem) => {
-		this.setState({
-			selectedItem,
-		});
-	};
-	render() {
-		const { selectedItem } = this.state;
+
+import React from "react";
+import { StarshipList } from "../sw-components";
+import { withRouter } from "react-router-dom";
+const StarshipPage=({history,match})=> {
 		return (
-			<Row
-				left={<StarshipList onItemSelected={this.onItemSelected} />}
-				right={<StarshipDetails itemId={selectedItem} />}
-			/>
+			<StarshipList onItemSelected={(itemId)=>{
+				return history.push(`starship/${itemId}`);
+			}}/>
 		);
-	}
-}
+		}
+	
+export default withRouter(StarshipPage);
